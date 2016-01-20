35646686e6a1bf314b8b9a7760e98aad43d27f8d
Patrick Eli Catach <patrick.catach@gmail.com>
2016-01-20 17:14:43 -0200

    mais caracteristicas atualizadas

--- client/javascript/especie.js
+++ client/javascript/especie.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
-  $.getJSON("/api/Species/03d2e1d25d805f51bd65ea02012e14a7", {}, function(data){
+  //$.getJSON("/api/Species/89ce2d9a768ac2605cc1d82df590dd54", {}, function(data){
+  $.getJSON("/api/Species/16e63286e2bc9d76a5ea2718a5109a43", {}, function(data){
     $.getJSON("/api/Specimens/" + data.specimens[0].id, {}, function(specimen){
       // titulo
       document.title = specimen["dwc:scientificName"].value + " " + specimen["dwc:scientificNameAuthorship"].value;
@@ -13,6 +14,21 @@ $(document).ready(function(){
       $("#periodoDeFloracao").append(specimen["rcpol:floweringPeriod"].value); //TODO
 
       // caracteristicas da flor
+      $("#sindromeDePolinizacao").append(data["rcpol:pollinationSyndrome"].value);
+      $("#unidadeDeAtracao").append(data["rcpol:attractionUnit"].value);
+      $("#sexualidade").append(data["rcpol:flowerSexuality"].value);
+      $("#tamanhoDaFlor").append(data["rcpol:flowerSize"].value);
+      $("#forma").append(data["rcpol:flowerForm"].value);
+      $("#simetria").append(data["rcpol:flowerSymmetry"].value);
+      $("#corDaFlor").append(data["rcpol:flowerColor"].value);
+      $("#antese").append(data["rcpol:flowerOpeningTime"].value);
+      $("#deiscenciaDaAntera").append(data["rcpol:antherDehiscence"].value);
+      $("#odor").append(data["rcpol:odorPresence"].value);
+      $("#tipoDeRecursoFloral").append(data["rcpol:mainFloralResourceCollecteByVisitors"].value);//TODO: collecteD
+
+      //TODO: imagens da planta
+
+      //TODO: Descrição Polínica
 
     });
   });
