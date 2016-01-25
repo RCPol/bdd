a20f695aaea9ce5c1046eb0db6dc6765db33c8ad
Allan Koch Veiga <allan.kv@gmail.com>
2016-01-25 14:13:24 -0500

    Middware e HTML templates para Species

--- server/server.js
+++ server/server.js
@@ -1,5 +1,8 @@
 var loopback = require('loopback');
 var boot = require('loopback-boot');
+var path = require('path');
+var mustache = require('mustache');
+var fs = require('fs');
 
 var app = module.exports = loopback();
 
@@ -15,6 +18,25 @@ app.start = function() {
     }
   });
 };
+app.use(loopback.compress());
+app.use(loopback.static(path.resolve(__dirname, '../client')));
+
+// Aqui vai ter que ser diferente porque a quantidade de parâmetros é variável. Vai ter que usar os parametros do tipo ?parmA=X&paramB=Y
+app.get('/:id', function(req, res) {
+  var template = fs.readFileSync('./client/index.mustache', 'utf8');
+  // var partials = {
+  //   item: fs.readFileSync('./client/item_partial.mustache', 'utf8')
+  // };
+  var params = {id: req.params.id};
+  res.send(mustache.render(template, params));
+});
+// Repetir para os outros profiles
+app.get('/profile/species/:id', function(req, res) {
+  var template = fs.readFileSync('./client/species.mustache', 'utf8');
+  var params = {id: req.params.id};
+  res.send(mustache.render(template, params));
+});
+
 
 // Bootstrap the application, configure models, datasources and middleware.
 // Sub-apps like REST API are mounted via boot scripts.
