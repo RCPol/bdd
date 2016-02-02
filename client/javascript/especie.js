f0cd17a64d3f787c542f879d12ba250152144a79
Patrick Eli Catach <patrick.catach@gmail.com>
2016-02-01 22:00:23 -0200

    descricao polinica

--- client/javascript/especie.js
+++ client/javascript/especie.js
@@ -8,28 +8,64 @@ function readSpecies(id){
       // nome da espécie
       $("#nomeDaEspecie").append(document.title);
 
-      //TODO: iterar nas propriedades de "data"
-      $("#habito").append(data["rcpol:habit"].value); //TODO: do for each state
+      escreverEstados("#habito", data["rcpol:habit"].states);
+
       $("#origem").append(specimen["dwc:establishmentMean"].value); //TODO: pular se nao tiver a informação
-      $("#periodoDeFloracao").append(specimen["rcpol:floweringPeriod"].value); //TODO janeiro e fevereiro e março e abril....
+
+      $("#periodoDeFloracao").append(specimen["rcpol:floweringPeriod"].months.join(', '));
 
       // caracteristicas da flor
-      $("#sindromeDePolinizacao").append(data["rcpol:pollinationSyndrome"].value);
-      $("#unidadeDeAtracao").append(data["rcpol:attractionUnit"].value);
-      $("#sexualidade").append(data["rcpol:flowerSexuality"].value);
-      $("#tamanhoDaFlor").append(data["rcpol:flowerSize"].value);
-      $("#forma").append(data["rcpol:flowerForm"].value);
-      $("#simetria").append(data["rcpol:flowerSymmetry"].value);
-      $("#corDaFlor").append(data["rcpol:flowerColor"].value);
-      $("#antese").append(data["rcpol:flowerOpeningTime"].value);
-      $("#deiscenciaDaAntera").append(data["rcpol:antherDehiscence"].value);
-      $("#odor").append(data["rcpol:odorPresence"].value);
-      $("#tipoDeRecursoFloral").append(data["rcpol:mainFloralResourceCollecteByVisitors"].value);//TODO: collecteD
+      escreverEstados("#sindromeDePolinizacao", data["rcpol:pollinationSyndrome"].states);
+      escreverEstados("#unidadeDeAtracao", data["rcpol:attractionUnit"].states);
+      escreverEstados("#sexualidade", data["rcpol:flowerSexuality"].states);
+      escreverEstados("#tamanhoDaFlor", data["rcpol:flowerSize"].states);
+      escreverEstados("#forma", data["rcpol:flowerForm"].states);
+      escreverEstados("#simetria", data["rcpol:flowerSymmetry"].states);
+      escreverEstados("#corDaFlor", data["rcpol:flowerColor"].states);
+      escreverEstados("#antese", data["rcpol:flowerOpeningTime"].states);
+      escreverEstados("#deiscenciaDaAntera", data["rcpol:antherDehiscence"].states);
+      escreverEstados("#odor", data["rcpol:odorPresence"].states);
+      escreverEstados("#tipoDeRecursoFloral", data["rcpol:mainFloralResourceCollecteByVisitors"].states); //TODO collecteD
 
       //TODO: imagens da planta
 
-      //TODO: Descrição Polínica
+      // Descrição Polínica
+      escreverEstados("#unidadeDeDispersaoDoPolen", data["rcpol:pollenDispersalUnit"].states);
+      //$("#tamanhoDoPolen").append(data["rcpol:pollenDiameter"].value); //TODO
 
+      var p = data["rcpol:polarAxis"];
+      $("#polarAxis").append(p.mean + "±" + p.sd + "(" + p.min + "-" + p.max + ")" );
+
+      var e = data["rcpol:equatorialAxis"];
+      $("#equatorialAxis").append(e.mean + "±" + e.sd + "(" + e.min + "-" + e.max + ")" );
+
+      escreverEstados("#simetriaDoPolen", data["rcpol:pollenSymmetry"].states);
+      escreverEstados("#polaridadeDoPolen", data["rcpol:pollenPolarity"].states);
+      escreverEstados("#ambitoDoPolen", data["rcpol:pollenAmbit"].states);
+      escreverEstados("#formaDoPolen", data["rcpol:pollenShape"].states);
+
+      var p_e = data["rcpol:pollenShapePE"];
+      $("#formaDoPolenPE").append(p_e.mean + "±" + p_e.sd + "(" + p_e.min + "-" + p_e.max + ")" );
+
+      escreverEstados("#tipoDeAberturaDoPolen", data["rcpol:pollenAperture"].states); //letra maiuscula
+      // primeira letra maiuscula após o ponto
+      $("#tipoDeAberturaDoPolen").css('textTransform', 'capitalize');
+      escreverEstados("#caracteristicaDoColpo", data["rcpol:colpeFeature"].states);
+      escreverEstados("#caracteristicaDoPoro", data["rcpol:poreFeature"].states);
+
+      var espexi = data["rcpol:espexi"];
+      $("#espexi").append(espexi.mean + "±" + espexi.sd + "(" + espexi.min + "-" + espexi.max + ")" );
+
+      escreverEstados("#ornamentacaoDaExina", data["rcpol:exineOrnamentation"].states);
     });
   });
 };
+
+function escreverEstados(seletor, estados){
+  // adicionar separador ","...
+  if (estados.length > 1){
+    $(seletor).append(estados.map(function(estado){ return estado.value; }).join(", "));
+  }
+  else
+    $(seletor).append(estados[0].value);
+}
