782d3f9ce7cd6ace2b67a41eac91bce830591bb6
Allan Koch Veiga <allan.kv@gmail.com>
2016-02-15 16:34:10 -0500

    Melhoria de desempenho
    Os dados do espécime só eram carregados depois do Mapa estar “pronto”,
    e estava demorando muito. Eu mudei para primeiro consumir a API depois
    carregar o mapa.

--- client/javascript/especime.js
+++ client/javascript/especime.js
@@ -1,34 +1,29 @@
-function readSpecimen(id, map){
+function readSpecimen(id, cb){
   $.getJSON("/api/Specimens/"+id, function(specimen){
-    var name = specimen["dwc:scientificName"].value + " " + specimen["dwc:scientificNameAuthorship"].value;
-    document.title = name;
+    specimenDb = specimen;
+    var name = specimenDb["dwc:scientificName"].value + " " + specimenDb["dwc:scientificNameAuthorship"].value;
+    document.title = "RCPol - "+name;
 
     $("#nome").append(name);
 
     //TODO: informações da palinoteca
     //TODO: links para ficha da palinoteca e ficha da especie
 
-    $("#coletor").append(specimen['dwc:recoredBy'].value); //TODO: recorDedBy
+    $("#coletor").append(specimenDb['dwc:recoredBy'].value); //TODO: recorDedBy
 
     //TODO: numero do coletor
     //TODO: nome do herbario
 
-    $("#numeroDeRegistroNoHerbario").append(specimen["dwc:catalogNumber"].value);
+    $("#numeroDeRegistroNoHerbario").append(specimenDb["dwc:catalogNumber"].value);
 
     //TODO: numero do registro na palinoteca
 
-    $("#pais").append(specimen['dwc:country'].value);
-    $("#estado").append(specimen['dwc:stateProvince'].value); //TODO: sigla
-    $("#municipio").append(specimen['dwc:municipality'].value);
-    $("#bioma").append(specimen['rcpol:vegetalFormationType'].value); //TODO: vegetalFormationType = bioma?
+    $("#pais").append(specimenDb['dwc:country'].value);
+    $("#estado").append(specimenDb['dwc:stateProvince'].value); //TODO: sigla
+    $("#municipio").append(specimenDb['dwc:municipality'].value);
+    $("#bioma").append(specimenDb['rcpol:vegetalFormationType'].value); //TODO: vegetalFormationType = bioma?
 
-    var p = [specimen["dwc:decimalLatitude"].value, specimen["dwc:decimalLongitude"].value];
-    $("#latitude").append(p[0]);
-    $("#longitude").append(p[1]);
-    var marker = L.marker(p, {opacity:0.9}).addTo(map);
-    map.setView(p, 17);
-
+    cb();
     //TODO: informações adicionais
-
   });
 }
