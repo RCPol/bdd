409a7b8a4c0bd6e56e04fd4e727f528d29b6d34d
Allan Koch Veiga <allan.kv@gmail.com>
2016-08-22 15:40:03 -0300

    [shchema] Tenta criar rezied e thumbnail mesmo que orginal exista

--- common/models/schema.js
+++ common/models/schema.js
@@ -266,7 +266,7 @@ module.exports = function(Schema) {
   //   }
   // };
 
-  var logs = {};
+
   //Método by Raquel
   Schema.downloadImages = function (cb) {
     //Schema aqui vai realizar uma consulta no banco de dados pegando os valores chave e valor do registro.
@@ -274,14 +274,14 @@ module.exports = function(Schema) {
     //Onde a imagem vai ser salva na pasta do cliente
     var startTime = new Date();
     Schema.find({where:{images:{exists:true}},fields:{id:true,images:true}}, function(err, results) {
-      var queue = {};
+      var queue = [];
       results.forEach(function(rec) {
         rec.images.forEach(function(img) {
-          queue[img.original] = img;
+          queue.push(img);
         });
       });
-      var downloader = new ImageDownloader(queue);
-      downloader.download().on("done",
+      var downloader = new ImageDownloader();
+      downloader.download(queue).on("done",
         function() {
           console.log("Terminou #"+downloader.count+" em "+(new Date().getTime() - startTime.getTime()));
           downloader.log.unshift("Tempo total: "+((new Date().getTime() - startTime.getTime())/1000)+"s");                    
@@ -292,38 +292,48 @@ module.exports = function(Schema) {
     });
 
   };
-  function ImageDownloader(queue) {
+  function ImageDownloader() {
     EventEmitter.call(this);
     this.log = [];
-    this.count = 0;
-    this.queue = queue;
+    this.count = 0;    
     this.requestErrorCount = 0;
   }
   util.inherits(ImageDownloader, EventEmitter);
-  ImageDownloader.prototype.download = function(cb) {
+  ImageDownloader.prototype.download = function(queue,cb) {    
     var self = this;
-    if (Object.keys(self.queue).length-1 == self.count){ // testa se terminou
+    if (queue.length == 0){ // testa se terminou
       self.log.unshift("Total de imagens: "+self.count)
       self.emit("done")
       return false;
     }
-    var image = new Image(self.queue[Object.keys(self.queue)[self.count]]); // Pega a chave do objeto na posição [count]
-    image.checkIfExist();
-    image.on("exists",
-        function() {
-          console.log("Existe "+image.local);
-          self.count++;
-          self.download();
-        }
-    ).on("doesNotExist",
-        image.requestFromURL
-    ).on("endDownload",
-        function() {
+    var image = new Image(queue.pop());     
+    image.checkIfExist(image.localPath,function(exists) {      
+      if(exists) image.emit("exists"); 
+      else image.emit("doesNotExist");
+    });
+    image.on("exists", function() {
+        console.log("Existe original "+image.local);        
+        self.count++;        
+        image.checkIfExist(image.thumbnailPath,function(exists) {
+          if(exists){      
+            console.log("Existe thumbnail "+image.thumbnailPath);              
+            image.checkIfExist(image.resizedPath,function(exists) {
+              if(exists) console.log("Existe resized "+image.resizedPath);
+              else image.emit("localFileWrote");
+            });
+          } else {
+            image.emit("localFileWrote");
+          }
+        });
+        self.download(queue,cb);
+      })
+    .on("doesNotExist",image.requestFromURL)
+    .on("endDownload", function() {
           image.writeLocalFile();
           self.count++
-          self.download();
-        }
-    ).on("localFileWrote",
+          self.download(queue,cb);
+      })
+    .on("localFileWrote",
       function() {
         image.convertResized();
         image.convertThumbnail();
@@ -332,7 +342,7 @@ module.exports = function(Schema) {
     );
     return this;
   };
-  function Image(img) {
+  function Image(img) {    
     EventEmitter.call(this);
     this.log = [];
     this.count = 0;
@@ -350,20 +360,18 @@ module.exports = function(Schema) {
     this.resizedPath = __dirname + "/../../client"+this.resized;
   }
   util.inherits(Image, EventEmitter);
-  Image.prototype.checkIfExist = function() {
+  Image.prototype.checkIfExist = function(path,cb) {
     var self = this;
-    fs.exists(self.localPath, function(exists){
-      if(exists)
-        return self.emit("exists")
-      return self.emit("doesNotExist");
+    fs.exists(path, function(exists){
+      cb(exists);        
     });
-    return this;
+    // return this;
   };
   Image.prototype.requestFromURL = function() {
     var self = this;
     request(self.original, {encoding: 'binary'}, function(err, response, body){
       if (err){
-        if (self.requestErrorCount==3) {
+        if (self.requestErrorCount==10) {
           console.log("Error to download "+self.original);
           self.requestErrorCount == 0;
           self.log.push("Error no download de "+self.original);
@@ -383,7 +391,7 @@ module.exports = function(Schema) {
     var self = this;
     fs.writeFile("client"+self.local, self.downloadedContent, 'binary', function(err){
         if(err){
-          if(self.writeLocalErrorCount==3){
+          if(self.writeLocalErrorCount==10){
             console.log("******** Local: "+self.local);
             console.log('Ops, um erro ocorreu!');
             console.log("URL: ",self.original);
