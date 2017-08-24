2bab8a40a2e270662dfdae8bf6b3a09cbebf3a63
Allan Koch Veiga <allan.kv@gmail.com>
2017-08-24 15:41:41 -0300

    migracao para loopback 3.x

--- node_modules/pkgcloud/lib/pkgcloud/amazon/index.js
+++
@@ -1,9 +0,0 @@
-/*
- * index.js: Top-level include for the AWS module.
- *
- * (C) 2012 Charlie Robbins, Ken Perkins, Ross Kukulinski & the Contributors.
- *
- */
-
-exports.compute = require('./compute');
-exports.storage = require('./storage');
