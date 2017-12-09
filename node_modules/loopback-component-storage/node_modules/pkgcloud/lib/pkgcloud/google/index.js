42db3da1e473612f0d6a1e2c60ed028d575820d6
Allan Koch Veiga <allan.kv@gmail.com>
2017-12-09 06:34:50 -0200

    remove modules

--- node_modules/loopback-component-storage/node_modules/pkgcloud/lib/pkgcloud/google/index.js
+++
@@ -1,8 +0,0 @@
-/*
- * index.js: Top-level include for the Google Cloud Storage module.
- *
- * (C) 2012 Charlie Robbins, Ken Perkins, Ross Kukulinski & the Contributors.
- *
- */
-
-exports.storage = require('./storage');
--- node_modules/pkgcloud/lib/pkgcloud/digitalocean/index.js
+++
@@ -1,8 +0,0 @@
-/*
- * index.js: Top-level include for the DigitalOcean module.
- *
- * (C) 2013 Charlie Robbins, Ken Perkins, Ross Kukulinski & the Contributors.
- *
- */
-
-exports.compute = require('./compute');
